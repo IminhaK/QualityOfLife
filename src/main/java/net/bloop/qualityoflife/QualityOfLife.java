41707ddd2000f426c7f4587f8174103a91505443
_Bloop <justinschrier@gmail.com>
2018-07-25 13:52:12 -0700

    Manual upload of first and second commit.
    Versions 1.0 and 1.1 included

---
+++ src/main/java/net/bloop/qualityoflife/QualityOfLife.java
@@ -0,0 +1,10 @@
+package net.bloop.qualityoflife;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = QualityOfLife.MODID, name = QualityOfLife.NAME, version = QualityOfLife.VERSION) public class QualityOfLife
+{
+    public static final String MODID = "qualityoflife";
+    public static final String NAME = "Quality Of Life";
+    public static final String VERSION = "1.1";
+}

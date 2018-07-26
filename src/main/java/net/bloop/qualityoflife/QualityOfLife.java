9383cde1c844af3e948311820413782c83737d16
bloop428 <justinschrier@gmail.com>
2018-07-26 12:14:22 -0700

    I should probably update the version. Oops.

--- src/main/java/net/bloop/qualityoflife/QualityOfLife.java
+++ src/main/java/net/bloop/qualityoflife/QualityOfLife.java
@@ -6,5 +6,5 @@ import net.minecraftforge.fml.common.Mod;
 {
     public static final String MODID = "qualityoflife";
     public static final String NAME = "Quality Of Life";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
 }

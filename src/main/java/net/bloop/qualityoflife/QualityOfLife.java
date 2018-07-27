ed662a7217bb17cdf03a255ddae9e153913fd3db
bloop428 <justinschrier@gmail.com>
2018-07-26 18:13:15 -0700

    Turns out the oredict was a no-no and it can be done in the recipe json.

--- src/main/java/net/bloop/qualityoflife/QualityOfLife.java
+++ src/main/java/net/bloop/qualityoflife/QualityOfLife.java
@@ -2,9 +2,10 @@ package net.bloop.qualityoflife;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = QualityOfLife.MODID, name = QualityOfLife.NAME, version = QualityOfLife.VERSION) public class QualityOfLife
+@Mod(modid = QualityOfLife.MODID, name = QualityOfLife.NAME, version = QualityOfLife.VERSION)
+public class QualityOfLife
 {
     public static final String MODID = "qualityoflife";
     public static final String NAME = "Quality Of Life";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.2.1";
 }

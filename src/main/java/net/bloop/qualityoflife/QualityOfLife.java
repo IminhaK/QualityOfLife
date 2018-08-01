048da4bb84ec8860263c89677eedd9e570d133ae
bloop428 <justinschrier@gmail.com>
2018-07-31 22:11:16 -0700

    How could I forget button uncrafting recipes?

--- src/main/java/net/bloop/qualityoflife/QualityOfLife.java
+++ src/main/java/net/bloop/qualityoflife/QualityOfLife.java
@@ -7,5 +7,5 @@ public class QualityOfLife
 {
     public static final String MODID = "qualityoflife";
     public static final String NAME = "Quality Of Life";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.4";
 }

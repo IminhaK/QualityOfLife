0fdfc5a6eba03fbf920b0336151f4f0a9b96af18
bloop428 <justinschrier@gmail.com>
2018-08-17 19:55:19 -0700

    more glass shard stuff

--- src/main/java/net/bloop/qualityoflife/RegistryEvents.java
+++ src/main/java/net/bloop/qualityoflife/RegistryEvents.java
@@ -1,7 +1,10 @@
 package net.bloop.qualityoflife;
 
 import net.bloop.qualityoflife.items.ItemGlassShard;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -17,6 +20,12 @@ public class RegistryEvents {
     {
         itemGlassShard = (ItemGlassShard)(new ItemGlassShard().setUnlocalizedName("itemGlassShard"));
         registerItem(itemGlassShard, "itemGlassShard");
+    }
+
+    @SubscribeEvent
+    public static void registerModels(ModelRegistryEvent event)
+    {
+        ModelLoader.setCustomModelResourceLocation(itemGlassShard, 0, new ModelResourceLocation(QualityOfLife.MODID + ":itemglassshard", "inventory"));
     }
 
     private static void registerItem(Item varItemName, String varName)
